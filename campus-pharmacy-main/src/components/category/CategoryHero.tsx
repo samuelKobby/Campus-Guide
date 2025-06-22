844a72444ccfc2d9578dfca5aee12c72a6af32ba
Samuel Gyasi Fordjour <sgyasifordjour@gmail.com>
2025-06-22 14:38:05 +0000

    Updated CategoryHero.tsx

--- campus-pharmacy-main/src/components/category/CategoryHero.tsx
+++ campus-pharmacy-main/src/components/category/CategoryHero.tsx
@@ -73,9 +73,11 @@ export const CategoryHero: React.FC<CategoryHeroProps> = ({
         </motion.div>
       </div>
 
-      {/* Decorative Elements */}
-      <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t from-gray-50 to-transparent" />
-      <div className="absolute -bottom-px left-0 w-full h-1 bg-gray-50" />
+       {/* Decorative Elements */}
+-      <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t from-gray-50 to-transparent" />
+-      <div className="absolute -bottom-px left-0 w-full h-1 bg-gray-50" />
++      <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t dark:from-gray-900 from-gray-50 to-transparent" />
++      <div className="absolute -bottom-px left-0 w-full h-1 dark:bg-gray-900 bg-gray-50" />
     </div>
   );
 };
